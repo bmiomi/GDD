38ba54544c4dcd14664432eb753d87864de5746d
bmiomi <bryanmiomi@hotmail.com>
2024-12-07 07:02:54 -0500

    s

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from core.app import MyApplication
 import sys
 
@@ -12,17 +11,3 @@ if __name__ == "__main__":
         print(f'Se encontro un error GRAVE QUE IMPIDE LA EJECUCION DEL PROGRAMA REPORTAR AL ADMINISTRADOR: {e}')
 
 
-=======
-from core.app import MyApplication
-
-if __name__ == "__main__":
-    try:
-        MyApplication.run()        
-    except ModuleNotFoundError as e:
-        print(f"hay un error faltan dependecias por instalar {e}")
-    except BaseException as e :
-        print(f'Se encontro un error GRAVE QUE IMPIDE LA EJECUCION DEL PROGRAMA REPORTAR AL ADMINISTRADOR: {e}')
-
-
-
->>>>>>> 8be410330e0e34aa49b9dec88801aabcfc683771
