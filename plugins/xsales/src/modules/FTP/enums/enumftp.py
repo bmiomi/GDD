884a053d064cdbd8d06f796ff8acfcb6138ac51f
bmino <bryanmiomi@hotmail.com>
2026-02-02 01:42:06 -0500

    Remove optional plugins from tracking

--- plugins/xsales/src/modules/FTP/enums/enumftp.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class Enumftp(Enum):
-
-    Validar_Maestros='Validar Maestros'
-
-    Validar_DESC='Validar DESC'
--- plugins/xsales/src/modules/Status/__init__.py
+++
@@ -1,3 +0,0 @@
-
-"""Paquete Status (arquitectura nueva)."""
-
